83aa8cbbb531d849d2a6e64fac1c2412dd105eed
Code Nation Student <codenationstudent@codes-mbp.home>
2020-05-04 16:44:31 +0100

    New HTML template

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
+// alert("Did this work?")
+
+/**
+ * 
+ * Select different elements and control what we want them to do
+ * Change the paragraph to a different colour... 
+ * We can do that with CSS... or JS.
+ * 
+ * Manipulate the elements and then display it... 
+ * 
+ */
+
 // document.getElementById("heading").style.color = "purple";
 // document.getElementById("heading").style.backgroundColor = "pink";
 
 const myFirstHeading = document.getElementById("heading");
 const button = document.getElementById("button");
-const input = document.getElementById|("input");
-const paragraphs = document.getElementsByTagName("hahaha");
-
+const input = document.getElementById("input");
+const paragraphs = document.getElementsByTagName("p"); // array
 
 paragraphs[0].style.color = "red";
+// charmaine has said a loop... 
+// FOR arguments sake... if we were to use a loop...
 
 for (let i = 0; i < paragraphs.length; i++){
     paragraphs[i].style.color = "red";
 }
 
-myFirstHeading.addEventListener("click", () =>{
+myFirstHeading.addEventListener("click", () => {
     myFirstHeading.style.color = "green";
 })
 
-button.addEventListener("click", () =>{
+button.addEventListener("click", () => {
     // myFirstHeading.style.color = "yellow";
     myFirstHeading.style.color = input.value;
-    paragraphs.style.color = input.value;
+    
 })
+
+
+
+/**
+ * three variables being created... so we can target them
+ * button that when clicked modifies the heading based on the input box value
+ */
